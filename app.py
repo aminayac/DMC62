b12e00a750ebd4da3ce77ef62f16bdd9c28b68b2
aminayac <aminayac@gmail.com>
2026-09-03 22:16:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Anibal Minaya")
 
 modulos = st.sidebar.selectbox("Seleccione el módulo",["Listas","Arreglos","Funciones","POO" ])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+elif modulos == "Funciones":
+  st.write("Te encuentras en el módulo de funciones")
+else:
+  st.write("Te encuentras en el módulo de POO")
