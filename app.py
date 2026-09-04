69059d148d94c058dae49ca7bfba49ad370b4a53
aminayac <aminayac@gmail.com>
2026-09-03 19:04:08 -0500

    Fix formatting of author name in app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Especializacion python for analytics")
 st.sidebar.title("Parámetros")
-st.write("Elaborado por. Anibal Minaya")
+st.write("Elaborado por: Anibal Minaya")
