52545ce2873bffb558232ce7a44596d00265881a
aminayac <aminayac@gmail.com>
2026-09-03 22:24:54 -0500

    Refactor app.py to improve code formatting

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ modulos = st.sidebar.selectbox("Seleccione el módulo",["Listas","Arreglos","Fun
 
 if modulos == "Listas":
   st.write("Te encuentras en el módulo de listas")
-  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango"),value=0)
-  valor_final = int(st.number_input("Ingresa tu valor final del rango"),value=10)
+  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango",value=0))
+  valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
   Lista = list(range(valor_inicial,valor_final))
   st.write(Lista)
   
