0eea99e785b4585b0b93abed4da6d869065685f1
aminayac <aminayac@gmail.com>
2026-09-08 22:29:20 -0500

    Update image display for Python logo
    Added width parameter to Python logo image.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Especializacion python for analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Anibal Minaya")
 
-st.image("Python_logo.png")
+st.image("Python_logo.png",width =300)
 
 st.sidebar.image("DMC.png")
 
