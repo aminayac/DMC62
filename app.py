3eff6fc908618b7298dd6103865d5743ab34df8a
aminayac <aminayac@gmail.com>
2026-09-10 22:21:00 -0500

    Refactor app.py to include new functions and inputs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import libreria_funciones as lf
 
 st.title("Especializacion python for analytics")
 st.sidebar.title("Parámetros")
@@ -28,5 +29,12 @@ elif modulos == "Arreglos":
 
 elif modulos == "Funciones":
   st.write("Te encuentras en el módulo de funciones")
+  capital_i = st.number.input("Ingrese el capital inicial",min_value=0,max_value=100000,value=1000)
+  aporte_m = st.number.input("Ingrese el aporte mensual",min_value=0,max_value=10000,value=100)
+  tasa_a = st.slider("Ingrese la tasa",min_value=0.01,max_value=1.00,value=0.05)
+  anios = st.slider("Ingrese la tasa",min_value=1,max_value=20,value=2)
+
+  resultado_valor_futuro=lf.valor_futuro_inversion(capital_i,aporte_m,tasa_a,anios)
+  st.write("El resultado de tu valor futuro de inversion es: ",resultado_valor_futuro)
 else:
   st.write("Te encuentras en el módulo de POO")
